7f1943a8ef116578658d0894a88d0797a1572371
Ccourter11 <courtercalvin@yahoo.com>
2021-06-10 15:49:06 -0500

    fixing urls

--- crittercatcher/urls.py
+++ crittercatcher/urls.py
@@ -20,12 +20,15 @@ from crittercatcherapi.views import register_user, login_user
 from crittercatcherapi.views import UsersView
 from rest_framework import routers
 
+
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'users', UsersView, 'user')
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('crittercatcher.urls')),
 ]
